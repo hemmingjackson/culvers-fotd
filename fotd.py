a590149713a43a63d3a1c024b484ac841e477432
Jackson <49170413+hemmingjackson@users.noreply.github.com>
2023-08-20 15:58:49 -0500

    formatting cleanup

--- fotd.py
+++ fotd.py
@@ -2,10 +2,7 @@ import requests, os, sys
 from bs4 import BeautifulSoup
 
 url_template = 'https://www.culvers.com/restaurants/{}'
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
-}
-
+headers = { 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3' }
 
 def get_fotd(city):
     url = url_template.format(city)
